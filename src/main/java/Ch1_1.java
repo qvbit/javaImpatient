c5201da6b70a17904ffbda89882dda20cc423562
nabeel <nabeelnau@gmail.com>
2022-05-03 07:33:49 +0100

    Finished ch1_1

--- src/main/java/Ch1_1.java
+++ src/main/java/Ch1_1.java
@@ -1,7 +1,15 @@
 package main.java;
 
+import java.util.Scanner;
+
 public class Ch1_1 {
     public static void main(String[] args) {
-        System.out.println("Ch1_1 code!");
+        var in = new Scanner(System.in);
+        var value = in.nextInt();
+        System.out.printf("Binary: %s\n", Integer.toBinaryString(value));
+        System.out.printf("Octal: %o\n", value);
+        System.out.printf("Hex: %x\n", value);
+        var recpiprocal = 1.0 / value;
+        System.out.printf("Hex floating point recip: %a\n", recpiprocal);
     }
 }
---
+++ src/main/java/Ch1_2.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class Ch1_2 {
+    public static void main(String[] args) {
+        System.out.println("Ch1_2 code!");
+    }
+}
